1c62237a95f7f0e356d56ca8a5ea93c933cb4e41
iworks <iworks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-03 08:43:21 +0000

    change version in trunk to 1.6.4
    git-svn-id: https://plugins.svn.wordpress.org/types/trunk@1018606 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- embedded/bootstrap.php
+++ embedded/bootstrap.php
@@ -131,7 +131,7 @@ function wpcf_embedded_init() {
     // Define necessary constants if plugin is not present
     // This ones are skipped if used as embedded code!
     if ( !defined( 'WPCF_VERSION' ) ) {
-        define( 'WPCF_VERSION', '1.6.3' );
+        define( 'WPCF_VERSION', '1.6.4' );
         define( 'WPCF_META_PREFIX', 'wpcf-' );
     }
 
