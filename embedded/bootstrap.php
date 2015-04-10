c5b96ad710bfef491c2c31d0c2c4e6591d1faebf
iworks <iworks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-10 07:52:01 +0000

    #RELEASE 1.6.6.2
    git-svn-id: https://plugins.svn.wordpress.org/types/trunk@1131826 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- embedded/bootstrap.php
+++ embedded/bootstrap.php
@@ -138,7 +138,7 @@ function wpcf_embedded_init() {
     // Define necessary constants if plugin is not present
     // This ones are skipped if used as embedded code!
     if ( !defined( 'WPCF_VERSION' ) ) {
-        define( 'WPCF_VERSION', '1.6.6.2' );
+        define( 'WPCF_VERSION', 'trunk' );
         define( 'WPCF_META_PREFIX', 'wpcf-' );
     }
 
